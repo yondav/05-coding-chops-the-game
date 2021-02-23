b3484acae6f311f4642a6480986a6436de7a4f6f
yondav <yonidavid.abr@gmail.com>
2021-02-23 02:54:44 -0500

    when user hits start,page changes to questions card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
-function startGame() {
-    
-}
+var start = document.getElementById("start-btn");
+var secondsLeft = document.querySelector(".time");
+var container = document.querySelectorAll(".container");
 
-function setNextQuestion {
+var timerCount;
 
-}
 
+container.forEach(function(containers) {
+    // console.log(containers);
+    start.addEventListener("click", function() {
+        container.forEach(function(containers) {
+            if(containers.classList.contains("two")) {
+                containers.classList.remove("hide");
+            }
+            if(containers.classList.contains("one")) {
+                containers.classList.add("hide");
+            }
+        });
+    });
+});
